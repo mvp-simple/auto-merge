00a61ea820488d32e8bbe51706625d136dce1e69
rinatusmanov <8078469@mail.ru>
2024-06-19 11:52:56 +0500

    step 1718779976

--- sync.sh
+++ sync.sh
@@ -8,11 +8,12 @@ do
       continue
     fi
     
+    echo ${item:0:-1}
+    exit 1
     cd $SCRIPT_DIR/$item; git pull
     mv $SCRIPT_DIR/${item}vendor $SCRIPT_DIR/${item}vendor_last || echo "" > /dev/null
     mv $SCRIPT_DIR/${item}.git $SCRIPT_DIR/${item}.git_last || echo "" > /dev/null
-    echo go mod vendor -o ../vendor${item:0:-1}
-    exit
+    echo go mod vendor -o ../vendor
     cd $SCRIPT_DIR/$item; go mod vendor -o ../vendor${item:0:-1}
     mv $SCRIPT_DIR/${item}vendor_last $SCRIPT_DIR/${item}vendor  || echo "" > /dev/null
 done
